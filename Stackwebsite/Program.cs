2f3f8916163c1ddb6699371beceaac7bdedbf2a8
bhupeshjamwal007 <bhupeshjamwal007@gmail.com>
2026-03-30 20:29:59 +0530

    Delete Stackwebsite/Program.cs

--- Stackwebsite/Program.cs
+++
@@ -1,8 +0,0 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
-
-// This tells the server to look for index.html in the "wwwroot" folder
-app.UseDefaultFiles();
-app.UseStaticFiles();
-
-app.Run();
